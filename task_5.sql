37eed443b63adcdd1273fdc8fcc12280ee78c0eb
sdksosot <aweal5834@gmail.com>
2025-08-18 01:03:31 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,10 @@
--- task_5.sql
--- Script to insert a single row into the customer table
+-- task_6.sql
+-- Script to insert multiple rows into the customer table
 
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
