a2398c1c7e5f41a06c0339de015099aa11d6a473
sdksosot <aweal5834@gmail.com>
2025-08-18 00:50:40 +0300

    Update Alx_boox_store.sql

--- Alx_boox_store.sql
+++ Alx_boox_store.sql
@@ -1,33 +1,34 @@
--- alx_book_store.sql
+-- إنشاء قاعدة البيانات
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
+-- استخدام قاعدة البيانات
 USE alx_book_store;
 
--- Authors Table
+-- إنشاء جدول Authors
 CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
-    author_name VARCHAR(215)
+    author_name VARCHAR(215) NOT NULL
 );
 
--- Books Table
+-- إنشاء جدول Books
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
-    title VARCHAR(130),
+    title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Customers Table
+-- إنشاء جدول Customers
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
     address TEXT
 );
 
--- Orders Table
+-- إنشاء جدول Orders
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
@@ -35,7 +36,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Order_Details Table
+-- إنشاء جدول Order_Details
 CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
