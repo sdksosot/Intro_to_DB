dea8858c69f07c8e71ee9274c1bb2a666f07bf90
sdksosot <aweal5834@gmail.com>
2025-08-18 00:58:35 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -24,9 +24,8 @@ CREATE TABLE Books (
 -- Table: Customers
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
     phone VARCHAR(20),
     address TEXT
 );
