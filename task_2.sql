8c7f0393fd4aa1210207e25185ef65d33ac3fcd6
sdksosot <aweal5834@gmail.com>
2025-08-18 00:54:21 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,43 @@
-CREATE TABLE Authors (...);
-CREATE TABLE Books (...);
-CREATE TABLE Customers (...);
-CREATE TABLE Orders (...);
-CREATE TABLE Order_Details (...);
+USE alx_book_store;
+
+-- AUTHORS TABLE
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- CUSTOMERS TABLE
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+-- BOOKS TABLE
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- ORDERS TABLE
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- ORDER_DETAILS TABLE
+CREATE TABLE IF NOT EXISTS order_details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
